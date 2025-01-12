338b1cf41af66e672d9f54dd2bbc81427cf8c50e
meetarora <meet.arora2403@gmail.com>
2025-01-12 11:00:27 +0530

    initial-commit

---
+++ enva2025/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>ENVA 2025</h1>
+          <p>LIGHTS.CAMERA.ACTION</p>
+          <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
