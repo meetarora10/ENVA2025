16efec4790afcbda941d9098b34a5f22555c8dd3
meetarora <meet.arora2403@gmail.com>
2025-01-16 10:57:28 +0530

    minor

--- enva2025/src/Components/LandingPage/LandingPage.jsx
+++ enva2025/src/Components/LandingPage/LandingPage.jsx
@@ -38,7 +38,7 @@ const LandingPage = () => {
           <img src="/ENVA-image.png" className="image" alt="" />
           <h1 className="heading">ENVA'25</h1>
         </div>
-        <button className="cta-button">EXPLORE EVENTS</button>
+        <button className="cta-button" onClick={()=>document.getElementById('events').scrollIntoView({behavior:'smooth'})}>EXPLORE EVENTS</button>
         <h1 className="dateSection">
           29<sup>th</sup>
           <span>JANUARY - </span>30<sup>th</sup>JANUARY
